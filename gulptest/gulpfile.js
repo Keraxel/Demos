90c24801a5c179f35cf78f2cf499ff64f6b1e6c3
Kamil Cieślak <kamil.cieslak@outlook.com>
2016-11-28 08:42:50 +0100

    Added linting LESS files

--- gulptest/gulpfile.js
+++ gulptest/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
     config = require('./gulp.config.js')(),
     secrets = require('./secrets.json'),
+    through = require('through'),
     $ = require('gulp-load-plugins')();
 
 gulp.task('default', ['build']);
@@ -10,7 +11,14 @@ gulp.task('watch', ['build'], () => {
     gulp.watch(config.lessBuildFiles, ['build:less']);
 });
 
-gulp.task('build:less', () => {
+gulp.task('lint:less', () => {
+        return gulp.src(config.lessBuildFiles)
+            .pipe($.lesshint())
+            .pipe($.lesshint.reporter())
+            .pipe(lesshintErrorReporter());
+    });
+
+gulp.task('build:less', ['lint:less'], () => {
     return gulp.src(config.lessBuildFiles)
         .pipe($.less())
         .pipe($.autoprefixer({
@@ -22,3 +30,25 @@ gulp.task('build:less', () => {
         .pipe($.cleanCss())
         .pipe(gulp.dest(config.buildCssPath));
 });
+
+function lesshintErrorReporter() {
+    var errorOccured = false;
+
+    return through(
+        function throughWrite(file) {
+            if (file.lesshint.resultCount > 0) {
+                errorOccured = true;
+            }
+        },
+        function throughEnd() {
+            if (errorOccured) {
+                this.emit('error', new $.util.PluginError('gulp-lesshint', {
+                    message: 'An error occured during linting.',
+                    showStack: false
+                }));
+            } else {
+                this.emit('end');
+            }
+        }
+    );
+}
